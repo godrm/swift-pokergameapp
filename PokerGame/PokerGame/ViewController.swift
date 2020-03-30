4192d01b4fa100cf0ad101718cd984644ddb61c3
Kim Jung <godrm77@gmail.com>
2020-03-30 11:49:58 +0900

    프로젝트 초기 설정 완료

---
+++ PokerGame/PokerGame/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PokerGame
+//
+//  Created by JK on 2020/03/30.
+//  Copyright © 2020 codesquad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var playerSegment: UISegmentedControl!
+    @IBOutlet weak var playerStack: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
